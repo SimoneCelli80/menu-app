4d2ea207c01680dfeb63b72ea2e3f9dc7cd2b55e
SimoneCelli80 <158849846+SimoneCelli80@users.noreply.github.com>
2024-06-21 09:36:10 +0200

    Tests for getting list of recipes added

--- src/test/java/com/sogeti/menu/app/service/RecipesServiceTest.java
+++ src/test/java/com/sogeti/menu/app/service/RecipesServiceTest.java
@@ -9,12 +9,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -89,6 +91,53 @@ public class RecipesServiceTest {
         assertEquals(expectedException.getMessage(), thrownException.getMessage());
 
         verify(recipesRepository).findById(nonexistentId);
+        verifyNoMoreInteractions(recipesRepository);
+    }
+
+    @Test
+    void givenAValidListOfIds_whenGettingMoreThanOneRecipe_thenAListOfRecipeDtosShouldBeReturned() {
+        RecipeDto recipeDto1 = RecipeFactory.aRecipeDto().build();
+        RecipeDto recipeDto2 = RecipeFactory.aRecipeDto().id(2L).build();
+        List<RecipeDto> recipeDtoList = new ArrayList<>();
+        recipeDtoList.add(recipeDto1);
+        recipeDtoList.add(recipeDto2);
+        List<RecipeEntity> recipeEntityList = new ArrayList<>();
+        RecipeEntity recipeEntity1 = RecipeFactory.aRecipe().build();
+        RecipeEntity recipeEntity2 = RecipeFactory.aRecipe().id(2L).build();
+        recipeEntityList.add(recipeEntity1);
+        recipeEntityList.add(recipeEntity2);
+        List<Long> idList = new ArrayList<>();
+        idList.add(1L);
+        idList.add(2L);
+
+        when(recipesRepository.findAllById(idList)).thenReturn(recipeEntityList);
+        List<RecipeDto> returnedRecipeDtoList = recipesService.getListOfRecipesByIds(idList);
+        assertEquals(recipeDtoList.getFirst().getId(), returnedRecipeDtoList.getFirst().getId());
+        assertEquals(recipeDtoList.getFirst().getRecipeName(), returnedRecipeDtoList.getFirst().getRecipeName());
+        assertEquals(recipeDtoList.getFirst().getIngredientList().size(), returnedRecipeDtoList.getFirst().getIngredientList().size());
+        assertEquals(recipeDtoList.getLast().getId(), returnedRecipeDtoList.getLast().getId());
+        assertEquals(recipeDtoList.getLast().getRecipeName(), returnedRecipeDtoList.getLast().getRecipeName());
+        assertEquals(recipeDtoList.getLast().getIngredientList().size(), returnedRecipeDtoList.getLast().getIngredientList().size());
+        verify(recipesRepository).findAllById(idList);
+        verifyNoMoreInteractions(recipesRepository);
+    }
+
+    @Test
+    void givenAListWithAtLeastANonexistentId_whenGettingMoreThanOneRecipe_thenA400ShouldBeThrown() {
+        List<Long> idList = new ArrayList<>();
+        idList.add(1L);
+        idList.add(1000L);
+        List<RecipeEntity> recipeEntityList = new ArrayList<>();
+        RecipeEntity recipeEntity1 = RecipeFactory.aRecipe().build();
+        recipeEntityList.add(recipeEntity1);
+        ResponseStatusException expectedException = new ResponseStatusException(HttpStatus.NOT_FOUND, "Sorry, 1 recipe has not be founded.");
+
+        when(recipesRepository.findAllById(idList)).thenReturn(recipeEntityList);
+        ResponseStatusException thrownException = assertThrows(ResponseStatusException.class, ()-> recipesService.getListOfRecipesByIds(idList));
+
+        assertEquals(thrownException.getStatusCode(), expectedException.getStatusCode());
+        assertEquals(thrownException.getMessage(), expectedException.getMessage());
+        verify(recipesRepository).findAllById(idList);
         verifyNoMoreInteractions(recipesRepository);
     }
 
